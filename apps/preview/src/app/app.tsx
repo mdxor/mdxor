d5125a08161cce15c8c458d2ecc609ae4d1f8128
Bowen7 <zwxdyx@foxmail.com>
2022-07-25 22:13:43 +0800

    add main app

--- apps/preview/src/app/app.tsx
+++ apps/preview/src/app/app.tsx
@@ -1,14 +1,20 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.less';
-import NxWelcome from './nx-welcome';
+import { VFileMessage } from 'vfile-message';
+import { compile } from '@mdx-js/mdx';
 
 export function App() {
-  return (
-    <>
-      <NxWelcome title="preview" />
-      <div />
-    </>
-  );
+  const handleClick = async () => {
+    try {
+      const res = await compile(`# Hello<>`);
+      console.log(res);
+    } catch (error) {
+      if (error instanceof VFileMessage) {
+        console.log(error.message);
+      }
+    }
+  };
+  return <button onClick={handleClick}>parse</button>;
 }
 
 export default App;
